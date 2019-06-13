5e6570f54ccfd128c4fac9bc00d05dd678e55fa8
Yuan Gao <example@email.com>
2019-06-13 13:28:30 -0700

    submit what I have now and resubmit later

--- src/main/java/com/yuangao/java401d4/songr/AlbumController.java
+++ src/main/java/com/yuangao/java401d4/songr/AlbumController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController {
@@ -24,5 +27,14 @@ public class AlbumController {
     public String getAddAlbumForm(){
        return "addAlbumForm";
     }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbum(@RequestParam String title , @RequestParam String artist, @RequestParam int songCount,
+                                 @RequestParam int length, @RequestParam String imageUrl){
+        Album album = new Album (title, artist,songCount,length,imageUrl);
+        albumRepository.save(album);
+        return new RedirectView("/albums");
+    }
+
 
 }
